0a0fbcea2897870e2a4fefd0bd6136e8c6e9deb2
Gali Nickel <gali.nickel95@gmail.com>
2021-02-01 23:06:10 +0200

    finalized version-removed lorem

--- js/service/proj-service.js
+++ js/service/proj-service.js
@@ -36,16 +36,16 @@ function renderPortfolio() {
     for (var i = 0; i < gProjList.length; i++) {
         var proj = getProjByID(i)
         strHTML += `    
-        <div class="col-md-4 col-sm-6 portfolio-item">
+        <div class="col-md-4 col-sm-6 portfolio-item ">
             <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1" id="${proj.id}" onclick="renderProjModal(id)" >
             <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
                 </div>
             </div>
-            <img class="img-fluid" src="img/portfolio/0${proj.id}-thumbnail.jpg" alt="">
+            <img class="img-fluid " src="img/portfolio/0${proj.id}-thumbnail.jpg" alt="">
             </a>
-            <div class="portfolio-caption">
+            <div class="portfolio-caption hover-shadow">
             <h4>${proj.title}</h4>
             <p class="text-muted">${proj.shortDesc}</p>
             </div>
